d18547007bca2e27f656a3af7984dd8af493b7db
mash-97 <mahimnhd97@gmail.com>
2021-01-21 04:53:15 +0600

    some minor experiments

--- music_player.js
+++ music_player.js
@@ -1,8 +1,17 @@
+class Track{
+	constructor(name, artist, source, image_path){
+		this.name = name;
+		this.artist = artist;
+		this.source = source;
+		this.image_path = image_path;
+	}
+}
+
 class MusicPlayList{
   // track_list requires obj: name, artist, source, image
   // music_list_container requires a html div element.
-  constructor(track_list, music_list_container_dv){
-    this.track_list = track_list;
+  constructor(tracks, music_list_container_dv){
+    this.track_list = tracks;
     this.mlc_div = music_list_container_dv;
   }
 
@@ -14,6 +23,13 @@ class MusicPlayList{
   }
 
 }
+
+
 class MusicPlayer{
-  constructor()
+  constructor(track_name_div, track_artist_div, track_art)
+  {
+	}
 }
+
+
+
